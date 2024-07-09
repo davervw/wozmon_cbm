3811bb359c7466818036f63d1518971d6619901e
Dave Van Wagner <git@davevw.com>
2024-07-08 19:03:54 -0700

    integrate disassembler for use with wozmon
    usage:
    1500 R ABCD
    will disassemble address ABCD for 20 statements, and return to wozmon

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 export ACME=${USERPROFILE}/Downloads/acme0.97win/acme
 export VICE=${USERPROFILE}/Downloads/GTK3VICE-3.8-win64/bin
 ${ACME}/acme -f cbm -o wozmon.prg -l wozmon.lbl -r wozmon.lst wozmon.asm \
+&& ${ACME}/acme -f cbm -o vwas6502.prg -l vwas6502.lbl -r vwas6502.lst vwas6502.asm \
+&& cat wozmon.lbl vwas6502.lbl > debug.lbl \
 && ${VICE}/c1541 wozmon.d64 -attach wozmon.d64 8 -delete "wozmon sys5120" -write wozmon.prg "wozmon sys5120" \
-&& ${VICE}/x64sc -moncommands wozmon.lbl -autostart wozmon.d64 >/dev/null 2>&1 &
+&& ${VICE}/c1541 wozmon.d64 -attach wozmon.d64 8 -delete "vwas6502" -write vwas6502.prg "vwas6502" \
+&& ${VICE}/x64sc -moncommands debug.lbl -autostart wozmon.d64 >/dev/null 2>&1 &
--- vwas6502.asm
+++ vwas6502.asm
@@ -1,6 +1,15 @@
 ;; vwas6502.asm - interactive console 6502 assembler
 ;;
-;; >>> STATUS: work in progress, disassembles itself <<<
+;; >>> STATUS: work in progress, ported for use with wozmon_cbm <<<
+;;
+;; INSTRUCTIONS
+;;   LOAD"WOZMON SYS5120",8,1
+;;   SYS 5120
+;;   1500 R 1500
+;;
+;; Use WozMon run command to run disassembler with an address as an argument
+;; The first 20 statements at that address will be disassembled
+;; Repeat with other desired addresses.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; MIT License
@@ -27,61 +36,43 @@
 ;; SOFTWARE.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-*=$c000
-
-; kernal/system calls
-charout=$ffd2
+; global
+inputbuf=$0200
 
 ; zeropage
 ptr1=$fb ; and $fc
-ptr2=$fd ; and $fe
+ptr3=$fd ; and $fe
 tmp=$ff
 opidx=$22
 inidx=$23
-mode=$24
-size=$25
-ptr3=$26 ; and $27
+admode=$28
+size=$29
+counter=$2A
 
-; test
+*=$1500
 start:
-    jmp +
+    jmp disassemble
 
-test: ; all the addressing modes here for testing disassembly
-    nop
-    lda $1234
-    lda $1234,x
-    lda $1234,y
-    asl
-    lda #$12
-    lda ($12,x)
-    lda ($12),y
-    jmp ($1234)
--   bne -
-    lda $12
-    lda $12,x
-    ldx $12,y
-    !byte $FF ; unknown
-
-+   lda #<start
-    ldx #>start
-    sta ptr1
-    stx ptr1+1
-    lda #<end
-    ldx #>end
-    sta ptr2
-    stx ptr2+1
-    jsr disassemble
-    lda ptr2
-    ldx ptr2+1
-    sta ptr1
-    stx ptr1+1
-    lda #<finish
-    ldx #>finish
-    sta ptr2
-    stx ptr2+1
-    jmp display_memory
+charout:
+    jmp $ffd2 ; allow patching
 
 disassemble:
+    lda #0
+    sta counter ; assume parse failed
+    jsr ignorespc
+    bcs ++
++   jsr inputhexword ; parse input to address word in ptr1
+    bcs ++
+    inc counter ; parse succeeded!
+++  lda #<copyright
+    ldx #>copyright
+    jsr strout
+    lda counter ; bool whether parsed input correctly or not
+    beq ++ ; branch if zero
+
+    lda #20
+    sta counter    ; setup to display 20 lines of disassembly
+
 -   ldy #0
     lda (ptr1),y
     jsr find_opcode
@@ -94,20 +85,12 @@ disassemble:
     sta ptr1
     bcc +
     inc ptr1+1
-+   jsr compareptrs
-    bcc -
-    rts
++   dec counter
+    bne -
+++  jmp $1400 ; return to wozmon
 
-compareptrs:
-    lda ptr1+1
-    cmp ptr2+1
-    bne +
-    lda ptr1
-    cmp ptr2
-+   rts
-
-find_opcode: ; INPUT: .A opcode byte, OUTPUT: C flag set if found, .A instruction index, .X opcode index, .Y mode, otherwise C clear, and .A/.X/.Y all $FF
-; and properties updated in ZP globals size,inidx,opidx,mode
+find_opcode: ; INPUT: .A opcode byte, OUTPUT: C flag set if found, .A instruction index, .X opcode index, .Y admode, otherwise C clear, and .A/.X/.Y all $FF
+; and properties updated in ZP globals size,inidx,opidx,admode
     ldy #1
     sty size
     ldy #nopcodes
@@ -133,7 +116,7 @@ find_opcode: ; INPUT: .A opcode byte, OUTPUT: C flag set if found, .A instructio
 +   sec
 ++  sta inidx
     stx opidx
-    sty mode
+    sty admode
     rts
 
 disp_opcode: ; .A opcode byte
@@ -161,21 +144,23 @@ disp_current:
     lda ptr1
     ldx ptr1+1
     jsr disphexword
-    lda #$20
-    jsr charout
+    jsr spaceout
     ldy #0
     ldx size
 -   lda (ptr1),y
     jsr disphexbyte
-    lda #$20
-    jsr charout
+    jsr spaceout
     iny
     dex
     bne -
 -   cpy #3
     beq +
+    lda #0
+    sta 199
+    jsr spaceout
+    lda #0
+    sta 199
     lda #$20
-    jsr charout
     jsr charout
     jsr charout
     iny
@@ -189,7 +174,7 @@ disp_current:
     jmp charout
 
 disp_mode
-    lda mode
+    lda admode
     cmp #13
     bcs +
     asl
@@ -353,44 +338,81 @@ disphexnybble: ; .A 0..F
     adc #$06
 +   jmp charout
 
-display_memory:
---  lda ptr1
-    ldx ptr1+1
-    jsr disphexword
-    lda #$20
-    jsr charout
--   jsr compareptrs
-    bcs +
-    ldy #0
-    lda (ptr1),y
-    jsr disphexbyte
-    lda #$20
-    jsr charout
-+   inc ptr1
+ignorespc:
+    ; input pointer first points to R when execution comes from wozmon
+--  iny ; advance input pointer
+    beq + ; way too far
+    lda inputbuf,y
+    and #$7F
     bne +
-    inc ptr1+1
-+   lda ptr1
-    and #$07
-    bne -
-    lda #13
-    jsr charout
-    jsr compareptrs
-    bcc --
+-   sec ; error
+    rts
++   cmp #$20
+    beq --
+    clc ; okay
     rts
 
-; charout: ; for debugging, wait for scan line to pass over entire screen at least once
-;     jsr $ffd2
-;     pha
-; -   lda $d011
-;     bpl -
-; -   lda $d011
-;     bmi -
-; -   lda $d011
-;     bpl -
-; -   lda $d011
-;     bmi -
-;     pla
-;     rts
+inputhexword:
+    jsr inputhexbyte
+    bcs ++
+    sta ptr1 ; assume one byte
+    lda #0
+    sta ptr1+1 ; extend to 16 bits
+    jsr inputhexbyte
+    bcs +
+    ldx ptr1 ; two bytes so shift the bytes
+    stx ptr1+1
+    sta ptr1
++   clc
+++  rts
+
+inputhexbyte:
+    jsr inputhexnybble
+    bcs +
+    sta tmp
+    jsr inputhexnybble
+    bcs +
+    asl tmp
+    asl tmp
+    asl tmp
+    asl tmp
+    ora tmp
++   rts
+
+inputhexnybble:
+    lda $0200,y
+    and #$7F
+    sec
+    sbc #$30
+    bcc ++
+    cmp #10
+    bcc +
+    sbc #7
+    bcc ++
+    cmp #10
+    bcc ++
+    cmp #16
+    bcs ++
++   iny
+    rts
+++  sec
+    rts
+
+strout:
+    sta ptr3
+    stx ptr3+1
+    ldy #0
+-   lda (ptr3),y
+    beq +
+    jsr charout
+    iny
+    bne -
++   rts
+
+spaceout:
+    jsr $14ed ; wozmon_cbm space out, use reverse for Vic-20 due to column constraints
+    lda #$20
+    rts
 
 end: brk
 
@@ -421,5 +443,7 @@ nopcodes = 151
 opcodes !byte $00,$01,$05,$06,$08,$09,$0A,$0D,$0E,$10,$11,$15,$16,$18,$19,$1D,$1E,$20,$21,$24,$25,$26,$28,$29,$2A,$2C,$2D,$2E,$30,$31,$35,$36,$38,$39,$3D,$3E,$40,$41,$45,$46,$48,$49,$4A,$4C,$4D,$4E,$50,$51,$55,$56,$58,$59,$5D,$5E,$60,$61,$65,$66,$68,$69,$6A,$6C,$6D,$6E,$70,$71,$75,$76,$78,$79,$7D,$7E,$81,$84,$85,$86,$88,$8A,$8C,$8D,$8E,$90,$91,$94,$95,$96,$98,$99,$9A,$9D,$A0,$A1,$A2,$A4,$A5,$A6,$A8,$A9,$AA,$AC,$AD,$AE,$B0,$B1,$B4,$B5,$B6,$B8,$B9,$BA,$BC,$BD,$BE,$C0,$C1,$C4,$C5,$C6,$C8,$C9,$CA,$CC,$CD,$CE,$D0,$D1,$D5,$D6,$D8,$D9,$DD,$DE,$E0,$E1,$E4,$E5,$E6,$E8,$E9,$EA,$EC,$ED,$EE,$F0,$F1,$F5,$F6,$F8,$F9,$FD,$FE
 instidx !byte $0A,$22,$22,$02,$24,$22,$02,$22,$02,$09,$22,$22,$02,$0D,$22,$22,$02,$1C,$01,$06,$01,$27,$26,$01,$27,$06,$01,$27,$07,$01,$01,$27,$2C,$01,$01,$27,$29,$17,$17,$20,$23,$17,$20,$1B,$17,$20,$0B,$17,$17,$20,$0F,$17,$17,$20,$2A,$00,$00,$28,$25,$00,$28,$1B,$00,$28,$0C,$00,$00,$28,$2E,$00,$00,$28,$2F,$31,$2F,$30,$16,$35,$31,$2F,$30,$03,$2F,$31,$2F,$30,$37,$2F,$36,$2F,$1F,$1D,$1E,$1F,$1D,$1E,$33,$1D,$32,$1F,$1D,$1E,$04,$1D,$1F,$1D,$1E,$10,$1D,$34,$1F,$1D,$1E,$13,$11,$13,$11,$14,$1A,$11,$15,$13,$11,$14,$08,$11,$11,$14,$0E,$11,$11,$14,$12,$2B,$12,$2B,$18,$19,$2B,$21,$12,$2B,$18,$05,$2B,$2B,$18,$2D,$2B,$2B,$18
 modeidx !byte $01,$03,$06,$06,$01,$02,$00,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A,$09,$03,$06,$06,$06,$01,$02,$00,$09,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A,$01,$03,$06,$06,$01,$02,$00,$09,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A,$01,$03,$06,$06,$01,$02,$00,$0C,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A,$03,$06,$06,$06,$01,$01,$09,$09,$09,$05,$04,$07,$07,$08,$01,$0B,$01,$0A,$02,$03,$02,$06,$06,$06,$01,$02,$01,$09,$09,$09,$05,$04,$07,$07,$08,$01,$0B,$01,$0A,$0A,$0B,$02,$03,$06,$06,$06,$01,$02,$01,$09,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A,$02,$03,$06,$06,$06,$01,$02,$01,$09,$09,$09,$05,$04,$07,$07,$01,$0B,$0A,$0A
+
+copyright !text 13,145,"VWAS2024 (C) 2024 DAVID R. VAN WAGNER", 13, "MIT LICENSE DAVEVW.COM", 157, 13, 0
 
 finish = *
